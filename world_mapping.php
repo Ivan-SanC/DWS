3732e4bcc82ea1957032c3cabbf605e808154264
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-10-25 15:22:58 +0200

    a

--- world_mapping.php
+++ world_mapping.php
@@ -4,31 +4,37 @@ $contents_cities = file_get_contents("https://dawsonferrer.com/allabres/apis_sol
 $cities = json_decode($contents_cities, true);
 $contents_countries = file_get_contents("https://dawsonferrer.com/allabres/apis_solutions/world.php?data=countries");
 $countries = json_decode($contents_countries, true);
-echo "<pre>";
-var_dump($contents_cities);
-echo "</pre>";
-echo "<pre>";
-var_dump($contents_countries);
-echo "</pre>";
 
 function mapCities()
 {
     global $cities;
     global $countries;
-
-    for($i=0;$i<count($cities);$i++){
-        for ($j=0;$j<count(($countries);$j++)){
-
+    for ($i = 0; $i < count($cities); $i++) {
+        for ($j = 0; $j < count($countries); $j++) {
+            if ($cities[$i]["CountryCode"] == $countries[$j]["Code"]) {
+                $cities[$i]["CountryName"] = $countries[$j]["Name"];
+            }
         }
     }
-
-
+    return $cities;
 }
 
 function mapCountries()
 {
     //TODO: Your code here
+    global $countries;
+    global $cities;
+    foreach ($countries as $countryKey => $countryValue){
+        foreach ($cities as $cityKey => $cityValue){
+            if($countryValue["Code"] == $cityValue["CountryCode"]){
+                $countries[$countryKey]["Cities"][$cityValue["ID"]] = $cityValue["Name"];
+            }
+        }
+    }
+    return $countries;
 }
 
-var_dump(mapCities());
+
+echo "<pre>";
 var_dump(mapCountries());
+echo "</pre>";
