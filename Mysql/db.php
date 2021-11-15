4ab0603e3253292ce2c9c315e95282bf70e05248
Ivan Sanchez <ivan.s.c@hotmail.com>
2021-11-15 16:24:28 +0100

    db en host

--- Mysql/db.php
+++ Mysql/db.php
@@ -9,10 +9,10 @@ $resultsJson = json_decode(file_get_contents($api_url . "results"), true);
 //CREAR UNA DB E INSERTAR DATOS EN LA DB
 
 //Mysql
-$servername = "localhost";
-$username = "root";
-$password = "admin";
-$dbname="db_elections";
+$servername = "sql480.main-hosting.eu";
+$username = "u850300514_isanchez";
+$password = "x43223947R";
+$dbname="u850300514_isanchez";
 
 
 // Create connection
@@ -25,15 +25,15 @@ if ($conn->connect_error) {
 echo "Connected successfully <br>";
 
 
-
 //Create DB
-$sql = "CREATE DATABASE IF NOT EXISTS db_elections;";
+$sql = "CREATE DATABASE IF NOT EXISTS u850300514_isanchez;";
 if ($conn->query($sql) === TRUE) {
     echo "Databases created successfully <br>";
 } else {
     echo "Error creating database: " . $conn->error;
 }
 $conn->close();
+
 
 
 // Create connection con db
@@ -43,6 +43,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully <br>";
+
+
 
 //Insert Partidos
 foreach ($partidosJson as $partidoJson){
