553e439704bde04765d44dab533790fe3e1b0010
Ivan Sanchez <ivan.s.c@hotmail.com>
2022-01-21 16:57:31 +0100

    hotels

--- mvc/Models/registerModel.php
+++ mvc/Models/registerModel.php
@@ -1,6 +1,21 @@
 <?php
-
+require_once "../Entities/user.php";
+require_once "../DB/dbo.php";
 class registerModel
 {
+protected dbo $db;
+
+    /**
+     * @param dbo $db
+     */
+    public function __construct(dbo $db)
+    {
+        $this->db = $db;
+    }
+
+    public function comprobarUser($email,$user){
+        $sql='SELECT * FROM table_users WHERE emailUser= "'.$email.'" OR nameUser= "'.$user.'"';
+
+    }
 
 }
