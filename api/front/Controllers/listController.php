e9f577c533d930482e3a20625a6f6cac860294b3
Ivan Sanchez <ivan.s.c@hotmail.com>
2022-02-07 16:13:50 +0100

    api

--- api/front/Controllers/listController.php
+++ api/front/Controllers/listController.php
@@ -1,5 +1,5 @@
 <?php
-$url="http://localhost/dws/api/back/Controllers/listController.php";
+$url="http://localhost/iSanchez/api/back/Controllers/listController.php";
 
 $hotels=json_decode(file_get_contents($url),true);
 session_start();
--- api/front/Controllers/singleHotelController.php
+++ api/front/Controllers/singleHotelController.php
@@ -1,6 +1,6 @@
 <?php
-$url="http://localhost/dws/api/back/Controllers/singleHotelController.php?id=";
-$urlEx="http://localhost/dws/api/back/Controllers/extrasController.php?id=";
+$url="http://localhost/iSanchez/api/back/Controllers/singleHotelController.php?id=";
+$urlEx="http://localhost/iSanchez/api/back/Controllers/extrasController.php?id=";
 
 $hotel=json_decode(file_get_contents($url.$_GET["id"]),true);
 $comments=json_decode(file_get_contents($urlEx.$_GET["id"]),true);
