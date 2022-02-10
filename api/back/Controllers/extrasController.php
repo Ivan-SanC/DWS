4e058b9058b7c85080db357096ec853e944e2409
Ivan Sanchez <ivan.s.c@hotmail.com>
2022-02-10 16:52:53 +0100

    api

--- api/back/Controllers/extrasController.php
+++ api/back/Controllers/extrasController.php
@@ -4,6 +4,9 @@ require_once "../Models/extrasModel.php";
 $extras = new extrasModel();
 if (isset($_GET["id"])) {
     $comments = $extras->getComments($_GET["id"]);
+    if(isset($_GET["comments"])){
+        $extras->insertComments($_GET["id"],$_GET["idUser"],$_GET["comments"]);
+    }
 } else {
     $comments = "ID NO SELECTED";
 }
