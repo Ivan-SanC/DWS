6a9e0c2db173a2ff2d3b856d47b45c024053c426
Ivan Sanchez <ivan.s.c@hotmail.com>
2022-02-10 16:59:35 +0100

    api

--- api/back/Controllers/singleHotelController.php
+++ api/back/Controllers/singleHotelController.php
@@ -3,6 +3,7 @@ require_once "../Models/singleHotelModel.php";
 
 
 $single = new singleHotelModel();
+$extras= new  extrasModel();
 
 if (isset($_GET["id"])) {
     $hotel = $single->getHotel($_GET["id"]);
--- api/front/Controllers/singleHotelController.php
+++ api/front/Controllers/singleHotelController.php
@@ -1,8 +1,11 @@
 <?php
 $url="http://localhost/iSanchez/api/back/Controllers/singleHotelController.php?id=";
 $urlEx="http://localhost/iSanchez/api/back/Controllers/extrasController.php?id=";
+session_start();
 
 $hotel=json_decode(file_get_contents($url.$_GET["id"]),true);
 $comments=json_decode(file_get_contents($urlEx.$_GET["id"]),true);
 $errorCode="";
+
+//pasa por get todos los datos que se recogen idHotel,idUser,checks
 require_once "../Views/singleHotelView.php";
